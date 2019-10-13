9889c01c0861a67e393b4eda2af636daa8256f36
WinZip <wydo@protonmail.com>
2019-10-13 08:54:50 +0300

    res

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import * as convert from "xml-js";
 
 
 export class Transformer {
+
     constructor(info) {
         this.info = info;
-    }
+    };
 
     xmlToJSON = () => {
         return convert.xml2json(this.info, {compact: true, spaces: 4});
-    }
+    };
 
     jsonToXML = () => {
         var options = {compact: true, ignoreComment: true, spaces: 4};
         return convert.json2xml(this.info, options);;
-    }
+    };
 
 }
 
@@ -26,7 +27,6 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
             result: ''
         };
@@ -37,8 +37,6 @@ class App extends Component {
     xmlToJSON = (info) => {
         let xml = info.currentTarget.form[0].value;
         let result = new Transformer(xml);
-        let test = result.xmlToJSON(xml);
-        debugger
         return this.setState({result: result.xmlToJSON(xml)})
     };
 
