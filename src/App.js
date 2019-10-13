cd67e6a5a1d8667b90053be6a3f9caebe32b5f56
WinZip <wydo@protonmail.com>
2019-10-13 08:53:38 +0300

    create

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as convert from "xml-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class Transformer {
+    constructor(info) {
+        this.info = info;
+    }
+
+    xmlToJSON = () => {
+        return convert.xml2json(this.info, {compact: true, spaces: 4});
+    }
+
+    jsonToXML = () => {
+        var options = {compact: true, ignoreComment: true, spaces: 4};
+        return convert.json2xml(this.info, options);;
+    }
+
+}
+
+
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            result: ''
+        };
+    }
+
+
+
+    xmlToJSON = (info) => {
+        let xml = info.currentTarget.form[0].value;
+        let result = new Transformer(xml);
+        let test = result.xmlToJSON(xml);
+        debugger
+        return this.setState({result: result.xmlToJSON(xml)})
+    };
+
+
+    jsonToXML = (info) => {
+        let json = info.currentTarget.form[0].value;
+        let result = new Transformer(json);
+        return this.setState({result: result.jsonToXML(json)})
+    };
+
+
+  render() {
+    return (
+        <div>
+          <h3>xml to json</h3>
+            <form>
+                <textarea></textarea>
+                <br/>
+                <button type="button" onClick={this.xmlToJSON}>xml to json</button>
+                <br/>
+
+                <button type="button" onClick={this.jsonToXML}>json to xml</button>
+            </form>
+
+            <div>
+                {this.state.result}
+            </div>
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, {Transformer} from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('xml to json', () => {
+    let xml = `<root>
+                 <B>3</B>
+                 <C>2</C>
+              </root>`;
+    let json = {"root":{"B":{"_text":"3"},"C":{"_text":"2"}}};
+    let result = new Transformer(xml);
+    expect(result.xmlToJSON(xml)).toBe(json)
+});
+
+it('json to xml', () => {
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<root>
+    <B>3</B>
+    <C>2</C>
+</root>`;
+    let json = { "_declaration": { "_attributes": { "version": "1.0", "encoding": "UTF-8" } }, "root": { "B": { "_text": "3" }, "C": { "_text": "2" } } };
+    let result = new Transformer(json);
+    expect(result.jsonToXML(json)).toBe(xml)
 });
